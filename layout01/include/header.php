0486bf53771793eb115a03ee45ca35db8bb3b73e
summerendless <46341018+summerendless@users.noreply.github.com>
2019-01-18 16:51:19 +0900

    index

---
+++ layout01/include/header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="ko">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/style.css">
+ </head>
+ <body>
+  <div class="wrap">
+	<header id="header" class="clearfix">logo
+		<ul class="clearfix">
+			<li><a href="#">menu1</a></li>
+			<li><a href="#">menu2</a></li>
+			<li><a href="#">menu3</a></li>
+		</ul>
+	</header>
+	<div class="container clearfix">
+		<section class="content">content</section>
+		<aside class="aside">aside</aside>
+	</div>
+	<footer id="footer">footer</footer>
+  </div>
+ </body>
+</html>
